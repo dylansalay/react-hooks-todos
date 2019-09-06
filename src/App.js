71c1ebc421410fe9c8cc7ac380c1445011ec2870
Dylan Salay <dylansalay@Dylans-MacBook-Pro.local>
2019-09-05 17:56:29 -0700

    complete todo

--- src/App.js
+++ src/App.js
@@ -28,6 +28,24 @@ function App() {
     }
   }
 
+  function createTodoAtIndex(e, i) {
+    const newTodos = [...todos];
+    newTodos.splice(i + 1, 0, {
+      content: '',
+      isCompleted: false,
+    });
+    setTodos(newTodos);
+    setTimeout(() => {
+      document.forms[0].elements[i + 1].focus();
+    }, 0);
+  }
+
+  function updateTodoAtIndex(e, i) {
+    const newTodos = [...todos];
+    newTodos[i].content = e.target.value;
+    setTodos(newTodos);
+  }
+
   function removeTodoAtIndex(i) {
     if (i === 0 && todos.length === 1) return;
     setTodos(todos => todos.slice(0, i).concat(todos.slice(i + 1, todos.length)));
@@ -36,23 +54,12 @@ function App() {
     }, 0);
   }
 
-  function createTodoAtIndex(e, i) {
-     const newTodos = [...todos];
-     newTodos.splice(i + 1, 0, {
-       content: '',
-       isCompleted: false,
-     });
-     setTodos(newTodos);
-     setTimeout(() => {
-       document.forms[0].elements[i + 1].focus();
-     }, 0);
-   }
+  function toggleTodoCompleteAtIndex(index) {
+    const temporaryTodos = [...todos];
+    temporaryTodos[index].isCompleted = !temporaryTodos[index].isCompleted;
+    setTodos(temporaryTodos);
+  }
 
-   function updateTodoAtIndex(e, i) {
-     const newTodos = [...todos];
-     newTodos[i].content = e.target.value;
-     setTodos(newTodos);
-   }
   return (
     <div className="app">
       <div className="header">
@@ -61,9 +68,18 @@ function App() {
       <form className="todo-list">
         <ul>
           {todos.map((todo, i) => (
-            <div className="todo">
-              <div className="checkbox" />
-              <input type="text" value={todo.content} onKeyDown={e => handleKeyDown(e, i)} />
+            <div className={`todo ${todo.isCompleted && 'todo-is-completed'}`}>
+              <div className={'checkbox'} onClick={() => toggleTodoCompleteAtIndex(i)}>
+                {todo.isCompleted && (
+                  <span>&#x2714;</span>
+                )}
+              </div>
+              <input
+                type="text"
+                value={todo.content}
+                onKeyDown={e => handleKeyDown(e, i)}
+                onChange={e => updateTodoAtIndex(e, i)}
+              />
             </div>
           ))}
         </ul>
